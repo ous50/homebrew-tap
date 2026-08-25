a8743450fbdf6c72ba84ecf142bb8d3df829c2b0
ous fifty <ousfifty@gmail.com>
2026-08-25 14:23:26 +0800

    maaend-beta 2.26.0-beta.5

--- Casks/maaend-beta.rb
+++ Casks/maaend-beta.rb
@@ -3,11 +3,11 @@ cask "maaend-beta" do
   os macos: "macos", linux: "linux"
   downloaded_file_format = on_system_conditional macos: "dmg", linux: "tar.gz"
 
-  version "2.23.0"
-  sha256  arm:          "9375446c52c2ba6c7cb0b2bdefa26041895d831638197a5f1a0367a7e1bcc117",
-          intel:        "6c2deffed531eb7e48f7c3bfd06ad792611fdf39445e379c4a51930c02ac73ab",
-          arm64_linux:  "9a1b7f3d60fd261189952b4ad400dfa47ab7fb96d6c9c9a82fde973072dfabc2",
-          x86_64_linux: "2c5e1e38668cff96204fc64f699646573db0643752da9a99dcf1e1612f3abb65"
+  version "2.26.0-beta.5"
+  sha256  arm:          "e3951bd959ef75e53a01b2cb6173f472cd83102222ac51b12219fc722164b9c8",
+          intel:        "e1c3efaefe9400858d19cf099f9a86257db1438f9070171c6ade84db29e0becf",
+          arm64_linux:  "1ecee906b1e423b9d2eb1f25edb4d07b56661dae794960d47b76e793f5cad164",
+          x86_64_linux: "69e932afd3e0fdf8a33869c096b2492e7766e4873709d81abf9632fb9a7046dc"
 
   on_macos do
     app "MaaEnd.app"
@@ -34,7 +34,6 @@ cask "maaend-beta" do
         EOS
       end
     end
-    "zh_CN"
   end
   language "en", default: true do
     desc "An Arknights:Endfield automation helper based on vision AI, in beta version."
@@ -48,7 +47,6 @@ cask "maaend-beta" do
         EOS
       end
     end
-    "en_US"
   end
 
   url "https://github.com/MaaEnd/MaaEnd/releases/download/v#{version}/MaaEnd-#{os}-#{arch}-v#{version}.#{downloaded_file_format}"
